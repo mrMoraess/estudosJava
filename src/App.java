270647b85cc395c60372d5cd0b2865019d66f5f6
mrMoraess <matheusrodriguesde347@gmail.com>
2024-07-26 20:39:46 -0300

    Tipos numericos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto1 = (short) numeroNormal;
     }
 }
