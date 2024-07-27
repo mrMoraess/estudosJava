691d8e39c55a47cb8cf06461cf14e4f608824ee8
mrMoraess <matheusrodriguesde347@gmail.com>
2024-07-27 12:49:49 -0300

    operadores

--- src/App.java
+++ src/App.java
@@ -5,5 +5,46 @@ public class App {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto1 = (short) numeroNormal;
+
+        numeroNormal = - numeroNormal;
+        System.out.println(numeroNormal);
+
+        numeroNormal = numeroNormal * (-1);
+        System.out.println(numeroNormal);
+
+        numeroNormal++;
+        System.out.println(numeroNormal --);
+        System.out.println(numeroNormal);
+
+        boolean verdade = true;
+
+        System.out.println(!verdade);
+
+        // ternary operator
+
+        int x, y;
+        x = 0;
+        y = 1;
+
+        System.out.println(x == y ? "Impossible" : "it is not true");
+
+        //
+
+        System.out.println(x != y);
+        System.out.println(x > y);
+        System.out.println(x >= y);
+
+        //
+        // usa-se .equals() para comparar objetos
+
+        String nome1 = "Matheus";
+        String nome2 = new String("Matheus");
+
+        System.out.println(nome1 == nome2);
+        System.out.println(nome1.equals(nome2));
+
+        // logic operators
+        // || or 
+        // && and
     }
 }
